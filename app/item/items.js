63890a1279175e7ffedfb563d11e471d68982771
Aleksander Lukasiewicz <aleksander_lukasiewicz@outlook.com>
2022-05-06 12:41:31 +0200

    Added crocus item

--- app/item/items.js
+++ app/item/items.js
@@ -8,10 +8,10 @@ let getGroups = () => [
     new Group("organic-group",[],"Organic collection",new Item(26,4,2022))
 ];
 let getItems = () => [
+    new Item(null,[],"https://www.instagram.com/p/CcIPscSKKyE/","/crocus","The Crocus","/crocus-base.webp","One of the first signs of spring - crocus, also know as the source of one of the most expensive spice.",new ItemDate(1,5,2022),undefined,["featured","organic-group"]),
     new Item(null, [], "https://www.instagram.com/p/CcIPscSKKyE/", "/lighthouse", "Lighthouse", "/lighthouse.webp", "Alone lighthouse on the night sea with a sky full of stars.", new ItemDate(16, 3, 2022), null, ["featured"]),
     new Item("lily", [], "https://www.instagram.com/p/CXlcDShooxu/", "/lily", "Water lily", "/lily-blank.webp", "Mandatory element in every organic picture of river or lake.", new ItemDate(10, 12, 2021), null, ["best","organic-group"]),
     new Item(null, [], "https://www.instagram.com/p/CPSx-X8hdVM/", "/tree-on-rock", "Organic pixels", "/tree-on-rock.webp", "One of the most iconic plants on the planet - a tree! Here growing on a giant rock.", new ItemDate(23, 5, 2021), null, ["best","organic-group"]),
-    new Item(null,[],"https://www.instagram.com/p/CcIPscSKKyE/","/crocus","The Crocus","/crocus-base.webp","One of the first signs of spring - crocus, also know as the source of on of the most rich spice.",new ItemDate(1,5,2022),undefined,["featured","organic-collection"]),
     new Item(null, [], "https://www.instagram.com/p/CMXFzXihSGw/", "/airship", "Ocean of clouds", "/airship-v2.webp", "The great adventure does not have to take place at sea. Now with new frame and clouds!", new ItemDate(27, 10, 2020), new ItemDate(13, 3, 2021), ["best"]),
     new Item(null, [], "https://www.instagram.com/p/COAYwduB3jc/", "/person", "Character practise", "/person_practise.webp", "The first result of learning to draw pixel characters. Based on tutorial by Slynyrd.", new ItemDate(23, 4, 2021), null, ["best"]),
     new Item(null, ["pixel-rose"], "https://www.instagram.com/p/CMZkmJaBhd1/", "/rose", "The digital blossom", "/rose_plant.webp", "A simple red rose. Slightly improved with new colours and details.", new ItemDate(9, 12, 2020), new ItemDate(14, 3, 2021), ["best","organic-group"]),
