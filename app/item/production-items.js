976bcb36aa566337afb413940f0375e6a6b4c3c9
Aleksander Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2022-05-17 16:34:52 +0200

    Items changes

--- app/item/production-items.js
+++ app/item/production-items.js
@@ -8,7 +8,7 @@ let getGroups = () =>
         new Group("home-group", ["home", "landing-beta"], "Home Group")
     ],
     getItems = () => [
-        new Item(null,[],"https://www.reddit.com/r/PixelArt/comments/ujkis3/the_crocus/","/crocus","The Crocus","/crocus-base.webp","One of the first signs of spring - crocus, also know as the source of one of the most expensive spice.",new ItemDate(1,5,2022),undefined,["featured","organic-group"]),
+        new Item(null,[],"https://www.instagram.com/p/CdqCxFOq1fh/","/crocus","The Crocus","/crocus-base.webp","One of the first signs of spring - crocus, also know as the source of one of the most expensive spice.",new ItemDate(1,5,2022),undefined,["featured","organic-group"]),
         new Item(null,[],"https://www.instagram.com/p/CcIPscSKKyE/","/lighthouse","Lighthouse","/lighthouse.webp","Alone lighthouse on the night sea with a sky full of stars.",new ItemDate(16,3,2022),null,["featured"]),
         new Item(null, [], "https://www.instagram.com/p/CXlcDShooxu/", "/lily/", "Water lily", "/lily-blank.webp", "Mandatory element in every organic picture of river or lake.", new ItemDate(10, 12, 2021), null, ["best"]),
         new Item(null, [], "https://www.instagram.com/p/CPSx-X8hdVM/", "/tree-on-rock", "Organic pixels", "/tree-on-rock.webp", "One of the most iconic plants on the planet - a tree! Here growing on a giant rock.", new ItemDate(23, 5, 2021), null, ["best"]),
