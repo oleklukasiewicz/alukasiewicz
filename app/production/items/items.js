ecc4c6555d7ff4ce6593425b585073bcc92390d9
Aleksander Lukasiewicz <aleksander_lukasiewicz@outlook.com>
2022-05-24 00:06:35 +0200

    Home group changes v2

--- app/production/items/items.js
+++
@@ -1,4 +0,0 @@
-const ITEM_VERSION = "0-0-0-4";
-const ITEM_ENVIROMENT = "production";
-let getGroups = () => ["home", "featured", "all", "organic"];
-let getItems = () => ["orchid", "crocus", "adventure", "airship", "isometric", "lighthouse", "lily", "person", "rose", "tree"];
