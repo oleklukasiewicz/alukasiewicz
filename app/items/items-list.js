adf238142fe7b4db805a71666f37fe252bbce44c
Aleksander Lukasiewicz <aleksander_lukasiewicz@outlook.com>
2022-05-24 00:24:37 +0200

    Structure changes v4

--- app/items/items-list.js
+++ app/items/items-list.js
@@ -1,4 +1,1 @@
-const ITEM_VERSION = "0-0-0-4";
-const ITEM_ENVIROMENT = "beta";
-let getGroups = () => ["home", "featured", "all", "organic"];
-let getItems = () => ["orchid", "crocus", "adventure", "airship", "isometric", "lighthouse", "lily", "person", "rose", "tree","beta"];
+const ITEM_VERSION="0-0-0-4";const ITEM_ENVIROMENT="production";let getGroups=()=>["all"];let getItems=()=>["orchid","crocus","adventure","airship","isometric","lighthouse","lily","person","rose","tree"];
