599b601e161d60b50e111db5902f3205d471eb9c
Aleksander Lukasiewicz <aleksander_lukasiewicz@outlook.com>
2022-08-23 21:16:47 +0200

    About banner performance improvemnets

--- app/js/main.js
+++ app/js/main.js
@@ -494,16 +494,6 @@ const profileView = new View(VIEW.profile, APP.url.profile, {}, {
     onNavigate: () => {
         window.scroll(0, 0);
         document.title = "About me - " + APP.name
-    },
-    onRegister: () => {
-        MultiLayerImageHelper(getById("about-img"), [
-            "/img/about-banner-layers/bg.webp",
-            "/img/about-banner-layers/bg-l1.webp",
-            "/img/about-banner-layers/bg-l2.webp",
-            "/img/about-banner-layers/stars-l1.webp",
-            "/img/about-banner-layers/stars-l2.webp",
-            "/img/about-banner-layers/tree.webp",
-        ]);
     }
 }, VIEW.profile, true, ViewController.loadingModes.never);
 const itemView = new View(VIEW.item, APP.url.item, { currentItem: null }, {
@@ -761,6 +751,7 @@ window.addEventListener("load", async function () {
         await ViewController.navigate(START_ROUTE.target, {
             routeArg: START_URL.slice(1, START_URL.length - 1)
         });
+    APP_NODE.classList.add(GLOBAL.loaded);
 });
 window.addEventListener("popstate", (event) => ViewController.navigateFromHistory(event.state));
 window.onresize = () => document.body.classList.toggle("scroll-fix", !isScrollbarVisible());
@@ -1082,38 +1073,6 @@ let ImageHelper = function (image, onload = () => { }, onerror = () => { }) {
         image.onload = () => resolve(onload(image));
         image.onerror = () => resolve(imageIsNotLoaded());
     });
-}
-let MultiLayerImageHelper = async function (baseNode, images = [], onload = () => { }, onerror = () => { }) {
-    let imageNotLoaded = function () {
-        let _errimg = document.createElement("IMG");
-        _errimg.src = "/img/image_error.webp";
-        baseNode.appendChild(_errimg);
-        onerror(baseNode);
-    }
-    let _someLayerNotLoaded = false;
-
-    images = images.map((image) => {
-        let _img = document.createElement("IMG");
-        _img.src = image;
-        baseNode.appendChild(_img);
-        return _img;
-    });
-    baseNode.classList.add(GLOBAL.loading);
-    await Promise.all(images.map(async (_img) => {
-        await new Promise((resolve) => {
-            _img.onload = () => resolve();
-            _img.onerror = () => {
-                _img.onload = function () { };
-                _someLayerNotLoaded = true;
-                resolve();
-            };
-        })
-    }));
-    baseNode.classList.replace(GLOBAL.loading,GLOBAL.loaded);
-    if (_someLayerNotLoaded)
-        imageNotLoaded();
-    else
-        onload();
 }
 
 //check if scrollbar is visible
