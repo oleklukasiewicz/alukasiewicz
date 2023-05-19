b4b8d5af6aa68f06eecf95feff02be75c5240e66
Aleksander Łukasiewicz <69370471+oleklukasiewicz@users.noreply.github.com>
2023-05-19 18:32:41 +0200

    Some deploy improvements 6

--- .vscode/to-production.bat
+++ .vscode/to-production.bat
@@ -4,11 +4,6 @@ set HOME=.\app
 set "HERE=%cd%"
 set VSCODE=%HERE%\.vscode
 
-call git checkout master .firebaserc
-call git checkout master .gitignore
-call git checkout master .github
-call git checkout master %HOME%
-
 call terser %HOME%\js\prerender.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\prerender.js
 call terser %HOME%\js\postrender.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\postrender.js
 call terser %HOME%\js\main.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\main.js
