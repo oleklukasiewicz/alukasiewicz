d8c8b27dc35656fa074dc5ddac478ac9cb25d29a
Aleksander Łukasiewicz <69370471+oleklukasiewicz@users.noreply.github.com>
2023-05-19 18:40:25 +0200

    Deploy improvements

--- .vscode/to-production.bat
+++ .vscode/to-production.bat
@@ -4,12 +4,20 @@ set HOME=.\app
 set "HERE=%cd%"
 set VSCODE=%HERE%\.vscode
 
-terser %HOME%\js\prerender.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\prerender.js
-terser %HOME%\js\postrender.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\postrender.js
-terser %HOME%\js\main.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\main.js
-terser %HOME%\js\item.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\item.js
+for /f "delims=" %%a in ('git rev-parse --abbrev-ref HEAD') do @set BRANCH=%%a
+if %BRANCH%==production (
 
-terser %HOME%\serviceworker.js -o %HOME%\serviceworker.js
+call terser %HOME%\js\prerender.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\prerender.js
+call terser %HOME%\js\postrender.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\postrender.js
+call terser %HOME%\js\main.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\main.js
+call terser %HOME%\js\item.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\item.js
 
+call terser %HOME%\serviceworker.js -o %HOME%\serviceworker.js
 
-terser %HOME%\item\storage.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json -o %HOME%\item\storage.js
+call terser %HOME%\item\storage.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json -o %HOME%\item\storage.js
+
+) ELSE (
+    echo Wrong branch
+)
+
+PAUSE
