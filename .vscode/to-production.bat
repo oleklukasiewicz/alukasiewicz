77b6c026810788f9ec67300989844f626704ac62
Aleksander Łukasiewicz <69370471+oleklukasiewicz@users.noreply.github.com>
2023-05-19 18:43:18 +0200

    Some deploy improvements 11

--- .vscode/to-production.bat
+++ .vscode/to-production.bat
@@ -1,14 +1,14 @@
 @echo off
-set HOME=.\app
+set HOME=./app
 
 set "HERE=%cd%"
-set VSCODE=%HERE%\.vscode
+set VSCODE=%HERE%/.vscode
 
-terser %HOME%\js\prerender.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\prerender.js
-terser %HOME%\js\postrender.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\postrender.js
-terser %HOME%\js\main.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\main.js
-terser %HOME%\js\item.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json  -o %HOME%\js\item.js
+terser %HOME%/js/prerender.js --config-file ./.vscode/terser-config.json --name-cache ./.vscode/terser-map.json  -o %HOME%/js/prerender.js
+terser %HOME%/js/postrender.js --config-file ./.vscode/terser-config.json --name-cache ./.vscode/terser-map.json  -o %HOME%/js/postrender.js
+terser %HOME%/js/main.js --config-file ./.vscode/terser-config.json --name-cache ./.vscode/terser-map.json  -o %HOME%/js/main.js
+terser %HOME%/js/item.js --config-file ./.vscode/terser-config.json --name-cache ./.vscode/terser-map.json  -o %HOME%/js/item.js
 
-terser %HOME%\serviceworker.js -o %HOME%\serviceworker.js
+terser %HOME%/serviceworker.js -o %HOME%/serviceworker.js
 
-terser %HOME%\item\storage.js --config-file .\.vscode\terser-config.json --name-cache .\.vscode\terser-map.json -o %HOME%\item\storage.js
+terser %HOME%/item/storage.js --config-file ./.vscode/terser-config.json --name-cache ./.vscode/terser-map.json -o %HOME%/item/storage.js
