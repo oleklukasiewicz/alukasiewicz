47214ec34af90aeecb51b6575271406971f4fabf
Olek Łukasiewicz <aleksander_lukasiewicz@outlook.com>
2024-07-11 23:11:59 +0200

    adjustments

--- app/js/postrender.js
+++ app/js/postrender.js
@@ -114,10 +114,9 @@ const SET_DEVICE_ORIENTATION = (coords) => {
   const profileImage = getById("profile-image");
   if (coords.x && coords.y) {
     const childs = profileImage.children;
-    console.log(coords,childs);
     Array.prototype.forEach.call(childs,(child) => {
       child.style.transform =
-        "translate(" + coords.y / 5 + "px," + (coords.x - 50) / 5 + "px)";
+        "translate(" + coords.y / 10 + "px," + (coords.x - 50) / 10 + "px)";
     });
   }
 };
